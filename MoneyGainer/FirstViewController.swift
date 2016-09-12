b2f8b6de266b753991369ec32ef77059e413e1b2
vietanh <vietanh@emotiv.vn>
2016-09-12 14:19:37 +0700

    Create project

---
+++ MoneyGainer/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MoneyGainer
+//
+//  Created by mac on 9/12/16.
+//  Copyright © 2016 VietAnh. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
